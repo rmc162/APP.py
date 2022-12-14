2df0f33d741375b355b37d9276f1b38d6fb1623e
rmc162 <117954106+rmc162@users.noreply.github.com>
2022-12-13 21:31:23 -0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+pip install sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
