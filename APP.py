02e21aeb9a9caa76bbc5e77dfd1e98ea347bb8e4
rmc162 <117954106+rmc162@users.noreply.github.com>
2022-12-13 19:02:49 -0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
