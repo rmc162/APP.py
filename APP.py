d83a3a4ee4c6189be1cce0b70ebfa2b2bdd069d9
rmc162 <117954106+rmc162@users.noreply.github.com>
2022-12-13 19:16:58 -0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.linear_model import LogisticRegression
+import sklearn
+
 
 s = pd.read_csv("social_media_usage.csv")
 
