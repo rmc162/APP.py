1d0362d42ccd39d55459b498c02d3898743e85cd
rmc162 <117954106+rmc162@users.noreply.github.com>
2022-12-13 19:12:13 -0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn as sklearn
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
