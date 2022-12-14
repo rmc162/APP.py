8d8e0901089ff442b12279096ca04098c4583e27
rmc162 <117954106+rmc162@users.noreply.github.com>
2022-12-13 19:37:20 -0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import scikit_learn
+import scikit-learn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
